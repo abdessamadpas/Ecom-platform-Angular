7998b0eed51b6d2c42c80799f6eb2046ee22c41c
AbdessamadPas <boughadoin@gmail.com>
2024-01-01 17:33:02 +0100

    home page UI done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +7,9 @@ import { MainCarouselComponent } from './home/main-carousel/main-carousel.compon
 import { HomeProductCardComponent } from './home/home-product-card/home-product-card.component';
 import { ProductSliderComponent } from './home/product-slider/product-slider.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavContentComponent } from './nav-content/nav-content.component';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -15,14 +17,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     MainCarouselComponent,
     HomeProductCardComponent,
-    ProductSliderComponent
+    ProductSliderComponent,
+    NavContentComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
